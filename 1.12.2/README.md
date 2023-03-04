e5102eade616930d09cc0b926bcd6d99046245a9
LunaStev <96914208+Quin0019@users.noreply.github.com>
2023-03-04 13:37:07 +0900

    Delete 1.12.2 directory

--- 1.12.2/README.md
+++
@@ -1,6 +0,0 @@
-IntelliJ
-
-
-1.12.2-14.23.5.2859
-
-`genIntellijRuns` -> `runClient`
--- 1.12.2/src/main/java/com/forge/sample/Reference.java
+++
@@ -1,7 +0,0 @@
-package com.forge.sample;
-
-public class Reference {
-    public static final String MODID = "forgesample";
-    public static final String NAME = "Forge_Sample";
-    public static final String VERSION = "1.0";
-}
